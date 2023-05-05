80872d1ccf6b14f9cd5a6ea264fa27f8c22c624a
Siddhartha Das <siddhartha@turbot.com>
2023-05-05 13:42:33 +0530

    Ignore not found error code.

--- steampipecloud/errors.go
+++ steampipecloud/errors.go
@@ -1,9 +1,24 @@
 package steampipecloud
 
 import (
+	"context"
 	"log"
 	"strings"
+
+	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
 )
+
+func shouldIgnoreErrors(notFoundErrors []string) plugin.ErrorPredicateWithContext {
+	return func(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData, err error) bool {
+		for _, pattern := range notFoundErrors {
+			// handle not found error
+			if strings.Contains(err.Error(), pattern) {
+				return true
+			}
+		}
+		return false
+	}
+}
 
 func shouldRetryError(err error) bool {
 	if strings.Contains(err.Error(), "429") {
